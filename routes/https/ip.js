387fe0ef3d2f4afbb2d07022b3f46aeec683d4e8
David Gatti <github@gatti.pl>
2018-07-12 10:11:15 -0700

    Look for the IP on Heroku #48

--- routes/https/ip.js
+++ routes/https/ip.js
@@ -2,13 +2,28 @@ let express = require('express');
 
 let router = express.Router();
 
+//
+//	This is an API endpoint that will send back the IP of the client making
+//	the request.
+//
 router.get('/', function(req, res, next) {
+
+	//
+	//	1. 	Get the public IP of the client making the request.
+	//
+	//		IMPORTANT
+	//
+	//			We are grabbing it from the header since this app is hosted
+	//			on Heroku, and our app is behind a Revers Proxy, so Heroku
+	//			passes this information in the header.
+	//
+	let ips = req.headers["x-forwarded-for"];
 
 	//
 	//	x.	Set the response
 	//
 	res.json({
-		ip: "131231232"
+		ip: ips
 	})
 
 	//
