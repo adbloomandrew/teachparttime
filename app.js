941de567e1f54a859d7c06740c2a4b7989183d60
David Gatti <github@gatti.pl>
2018-07-16 09:02:25 -0500

    Fix

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ app.use(helmet.contentSecurityPolicy({
 		frameSrc: ["'self'", "www.youtube.com"],
 		imgSrc: ["'self'", "data:", "*.addpipe.com"],
 		mediaSrc: ["'none'"],
-		objectSrc: ["'none'", "*.addpipe.com"],
+		objectSrc: ["*.addpipe.com"],
 		scriptSrc: ["'self'", "'unsafe-inline'", "*.addpipe.com", "cdnjs.cloudflare.com"],
 		styleSrc: ["'self'", "'unsafe-inline'", "fonts.googleapis.com"]
 	}
